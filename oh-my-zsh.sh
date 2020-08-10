f74a9f6f6d9d9c86a0902f39f3e8158bf1305b17
Southball <jamie.choi.mail.2002@gmail.com>
2020-08-10 17:06:35 +0800

    Delete oh-my-zsh.sh

--- oh-my-zsh.sh
+++
@@ -1,4 +0,0 @@
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-zsh -c 'git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"'
-zsh -c 'ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"'
-echo 'SPACESHIP_HOST_SHOW=always' >> ~/.zshrc
